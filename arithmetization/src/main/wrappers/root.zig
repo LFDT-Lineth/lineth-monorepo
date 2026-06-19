c051464c90cd82ed7bf1288501f2976c5ac6c297
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-06-19 10:39:06 +0000

    feat: add Poseidon2 permutation wrapper
    Signed-off-by: Ivo Kubjas <ivo.kubjas@consensys.net>

--- arithmetization/src/main/wrappers/root.zig
+++ arithmetization/src/main/wrappers/root.zig
@@ -1,3 +1,4 @@
 pub const custom_std = @import("custom_std.zig");
 pub const zkvm_types = @import("zkvm_types.zig");
 pub const keccak = @import("keccak.zig");
+pub const poseidon2 = @import("poseidon2.zig");
