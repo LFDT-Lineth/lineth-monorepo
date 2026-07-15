850751c6e6e6e0b03b523c36782b6bd24d8a697d
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-07-15 23:59:34 +0000

    test: compile zkc automatically
    Signed-off-by: Ivo Kubjas <ivo.kubjas@consensys.net>

--- prover-ray/zkcdriver/native_modules_test.go
+++ prover-ray/zkcdriver/native_modules_test.go
@@ -1,16 +1,11 @@
 package zkcdriver_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/utils/files"
 	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/wiop"
-	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/wiop/compilers/global"
-	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/wiop/compilers/localvanishing"
-	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/wiop/compilers/logderivativesum"
-	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/wiop/compilers/lookuptologderivsum"
-	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/wiop/compilers/nonnative"
-	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/wiop/compilers/rangecheck"
 	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/zkcdriver"
 )
 
@@ -21,26 +16,24 @@ func TestNative(t *testing.T) {
 	cases := []string{"u64", "u128", "u256"}
 	for i := range cases {
 		t.Run(cases[i], func(t *testing.T) {
-			zkcFilePath := zkcPath + cases[i] + ".bin"
 			zkcInputPath := zkcPath + cases[i] + ".json"
-			if files.CheckFilePath(zkcFilePath) != nil {
-				t.Fatalf("zkc file %s does not exist", zkcFilePath)
+			binf, err := compileBinaryConstraints(zkcPath + cases[i] + ".zkc")
+			if err != nil {
+				t.Fatalf("failed to compile zkc source: %v", err)
 			}
 			if files.CheckFilePath(zkcInputPath) != nil {
 				t.Fatalf("zkc input file %s does not exist", zkcInputPath)
 			}
+			binfBytes, err := binf.MarshalBinary()
+			if err != nil {
+				t.Fatalf("failed to marshal binary constraints: %v", err)
+			}
 			sys := wiop.NewSystemf("zkc-test/native/%s", cases[i])
 			sys.NewRound()
-			driver := zkcdriver.NewZkCDriver(sys, zkcdriver.Settings{}, files.MustRead(zkcFilePath))
+			driver := zkcdriver.NewZkCDriver(sys, zkcdriver.Settings{}, bytes.NewReader(binfBytes))
 			inputs := zkcdriver.ReadZkcInputs(zkcInputPath)
 
-			nonnative.Compile(sys)
-			rangecheck.Compile(sys)
-			lookuptologderivsum.Compile(sys)
-			logderivativesum.Compile(sys)
-			localvanishing.Compile(sys)
-			global.Compile(sys)
-			// pcs.Compile(sys)
+			proverCompilePipeline(sys)
 
 			proof, pi := sys.Prove(func(rt *wiop.Runtime) {
 				driver.AssignWithPreRead(rt, inputs)
