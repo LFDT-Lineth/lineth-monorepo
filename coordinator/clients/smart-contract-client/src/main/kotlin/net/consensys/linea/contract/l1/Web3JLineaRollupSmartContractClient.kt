30a32984c4231b9ae01e5ae8cf6055c9428f1b3f
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2026-07-23 00:28:51 +0100

    chore(coordinator): adds initial scafold for contract v9 RISC-V
    Signed-off-by: Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>

--- coordinator/clients/smart-contract-client/src/main/kotlin/net/consensys/linea/contract/l1/Web3JLineaRollupSmartContractClient.kt
+++ coordinator/clients/smart-contract-client/src/main/kotlin/net/consensys/linea/contract/l1/Web3JLineaRollupSmartContractClient.kt
@@ -2,7 +2,10 @@ package net.consensys.linea.contract.l1
 
 import linea.EthLogsSearcher
 import linea.contract.LineaRollupV6
+import linea.contract.l1.BlobsSubmissionV9
 import linea.contract.l1.BlockAndNonce
+import linea.contract.l1.FinalizationDataV9
+import linea.contract.l1.LineaRollupContractVersion
 import linea.contract.l1.LineaRollupSmartContractClient
 import linea.contract.l1.Web3JLineaRollupSmartContractClientReadOnly
 import linea.domain.BlobRecord
@@ -206,6 +209,58 @@ class Web3JLineaRollupSmartContractClient internal constructor(
             parentL1RollingHashMessageNumber,
           )
         web3jContractHelper.sendTransactionAfterEthCallAsync(function, BigInteger.ZERO, gasPriceCaps)
+          .thenApply { result -> result.transactionHash }
+      }
+  }
+
+  override fun submitBlobsV9(
+    blobData: BlobsSubmissionV9,
+    gasPriceCaps: GasPriceCaps?,
+    preflightWithEthCall: Boolean,
+  ): SafeFuture<String> {
+    val function = FunctionBuildersV9.buildSubmitBlobsFunctionV9(blobData)
+
+    return getVersion()
+      .thenCompose { version ->
+        if (version < LineaRollupContractVersion.V9) {
+          SafeFuture.failedFuture<String>(RuntimeException("Contract version=$version not supported"))
+        }
+        if (preflightWithEthCall) {
+          web3jContractHelper.executeBlobEthCall(
+            function = function,
+            blobs = blobData.blobs,
+            gasPriceCaps = gasPriceCaps,
+          )
+        } else {
+          SafeFuture.completedFuture<String>(null)
+        }
+      }.thenCompose {
+        web3jContractHelper.sendBlobCarryingTransactionAndGetTxHash(
+          function = function,
+          blobs = blobData.blobs,
+          gasPriceCaps = gasPriceCaps,
+        )
+      }
+  }
+
+  override fun finalizeBlocksV9(
+    data: FinalizationDataV9,
+    gasPriceCaps: GasPriceCaps?,
+    preflightWithEthCall: Boolean,
+  ): SafeFuture<String> {
+    val function = FunctionBuildersV9.buildFinalizeBlocksFunctionV9(data)
+
+    return getVersion()
+      .thenCompose { version ->
+        if (version < LineaRollupContractVersion.V9) {
+          SafeFuture.failedFuture<String>(RuntimeException("Contract version=$version not supported"))
+        }
+
+        if (preflightWithEthCall) {
+          web3jContractHelper.sendTransactionAfterEthCallAsync(function, BigInteger.ZERO, gasPriceCaps)
+        } else {
+          web3jContractHelper.sendTransactionAsync(function, BigInteger.ZERO, gasPriceCaps)
+        }
           .thenApply { result -> result.transactionHash }
       }
   }
--- coordinator/core-interfaces/src/main/kotlin/linea/contract/l1/LineaRollupSmartContractClient.kt
+++ coordinator/core-interfaces/src/main/kotlin/linea/contract/l1/LineaRollupSmartContractClient.kt
@@ -4,10 +4,51 @@ import linea.domain.BlobRecord
 import linea.domain.ProofToFinalize
 import linea.domain.gas.GasPriceCaps
 import tech.pegasys.teku.infrastructure.async.SafeFuture
+import kotlin.time.Instant
 
 data class BlockAndNonce(
   val blockNumber: ULong,
   val nonce: ULong,
+)
+
+data class BlobsSubmissionV9(
+  val blobs: List<ByteArray>,
+  val blobFinalBlockHashes: List<ByteArray>,
+  val parentShnarf: ByteArray,
+  val finalBlobShnarf: ByteArray,
+)
+
+data class ShnarfDataV9(
+  val parentShnarf: ByteArray,
+  val snarkHash: ByteArray,
+  val finalStateRootHash: ByteArray,
+  val dataEvaluationPoint: ByteArray,
+  val dataEvaluationClaim: ByteArray,
+)
+
+data class FinalizationDataV9(
+  val aggregatedProof: ByteArray,
+  val proofType: Int,
+  val parentStateRootHash: ByteArray,
+  val parentBlockHash: ByteArray,
+  val endBlockNumber: ULong,
+  val shnarfData: ShnarfDataV9,
+  val lastFinalizedTimestamp: Instant,
+  val finalTimestamp: Instant,
+  val lastFinalizedL1RollingHash: ByteArray,
+  val l1RollingHash: ByteArray,
+  val lastFinalizedL1RollingHashMessageNumber: ULong,
+  val l1RollingHashMessageNumber: ULong,
+  val l2MerkleTreesDepth: Int,
+  val lastFinalizedForcedTransactionNumber: ULong,
+  val finalForcedTransactionNumber: ULong,
+  val lastFinalizedForcedTransactionRollingHash: ByteArray,
+  val finalBlockHash: ByteArray,
+  val finalBlobHash: ByteArray,
+  val l2MerkleRoots: List<ByteArray>,
+  val filteredAddresses: List<ByteArray>,
+  val verifierKeys: List<ByteArray>,
+  val l2MessagingBlocksOffsets: ByteArray,
 )
 
 interface LineaSmartContractClient : LineaSmartContractClientReadOnly {
@@ -19,6 +60,7 @@ interface LineaSmartContractClient : LineaSmartContractClientReadOnly {
    */
   fun updateNonceAndReferenceBlockToLastL1Block(): SafeFuture<BlockAndNonce>
 
+  // TODO: not used, shall be removed
   fun finalizeBlocksEthCall(
     aggregation: ProofToFinalize,
     aggregationLastBlob: BlobRecord,
@@ -55,6 +97,26 @@ interface LineaRollupSmartContractClient :
    * Submit a list of blobs to the smart contract, with EIP4844 transaction
    */
   fun submitBlobs(blobs: List<BlobRecord>, gasPriceCaps: GasPriceCaps?): SafeFuture<String>
+
+  /**
+   * Submits blocks for V9 (RISC-V arithmetization)
+   * @param preflightWithEthCall when true will call eth_call to prevalidate the transaction does not revert
+   */
+  fun submitBlobsV9(
+    blobData: BlobsSubmissionV9,
+    gasPriceCaps: GasPriceCaps?,
+    preflightWithEthCall: Boolean = true,
+  ): SafeFuture<String>
+
+  /**
+   * Finalizes blocks for V9 (RISC-V arithmetization)
+   * @param preflightWithEthCall when true will call eth_call to prevalidate the transaction does not revert
+   */
+  fun finalizeBlocksV9(
+    data: FinalizationDataV9,
+    gasPriceCaps: GasPriceCaps?,
+    preflightWithEthCall: Boolean = true,
+  ): SafeFuture<String>
 }
 
 interface LineaValidiumSmartContractClient :
