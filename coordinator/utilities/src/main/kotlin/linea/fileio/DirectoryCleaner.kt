29f61c76ef5c8acb0f63df270c27108e9ad58cf1
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2026-07-23 15:12:04 +0100

    fix(coordinator): DirectoryCleaner
    Signed-off-by: Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>

--- coordinator/utilities/src/main/kotlin/linea/fileio/DirectoryCleaner.kt
+++ coordinator/utilities/src/main/kotlin/linea/fileio/DirectoryCleaner.kt
@@ -40,10 +40,9 @@ class DirectoryCleaner(
               vertx.fileSystem()
                 .delete(filePath)
                 .toSafeFuture()
-                .whenComplete { _, deleteException ->
-                  deleteException?.also { log.warn("Failed to delete $filePath", it) }
+                .handleException { deleteException ->
+                  log.warn("Failed to delete $filePath", deleteException)
                 }
-                .thenApply { }
             deletions.add(deletion)
           }
         }
