3530f2cef08f0629be7d5d1a43291156084976c1
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2026-07-23 15:03:09 +0100

    fix(coordinator): small fix on start/stop handlers
    Signed-off-by: Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>

--- coordinator/utilities/src/main/kotlin/linea/fileio/DirectoryCleaner.kt
+++ coordinator/utilities/src/main/kotlin/linea/fileio/DirectoryCleaner.kt
@@ -31,7 +31,7 @@ class DirectoryCleaner(
 
   internal fun cleanDirectory(path: Path): SafeFuture<*> {
     return vertx.fileSystem().readDir(path.toString()).toSafeFuture()
-      .thenApply { filePaths ->
+      .thenCompose { filePaths ->
         val deletions = mutableListOf<SafeFuture<*>>()
         filePaths.forEach { filePath ->
           val file = File(filePath)
