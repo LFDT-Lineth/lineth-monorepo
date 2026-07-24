a00ca42377835bd89667e9faf21944f4f09dd9f1
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-07-24 08:50:05 +0000

    tests: use zkcdriver test utilities for test case handling
    Signed-off-by: Ivo Kubjas <ivo.kubjas@consensys.net>

--- prover-ray/zkcdriver/native_modules_test.go
+++ prover-ray/zkcdriver/native_modules_test.go
@@ -1,12 +1,10 @@
 package zkcdriver_test
 
 import (
-	"bytes"
+	"os"
 	"testing"
 
 	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/utils/files"
-	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/wiop"
-	"github.com/LFDT-Lineth/lineth-monorepo/prover-ray/zkcdriver"
 )
 
 func TestNative(t *testing.T) {
@@ -17,29 +15,28 @@ func TestNative(t *testing.T) {
 	for i := range cases {
 		t.Run(cases[i], func(t *testing.T) {
 			zkcInputPath := zkcPath + cases[i] + ".json"
-			binf, err := compileBinaryConstraints(zkcPath + cases[i] + ".zkc")
+			zkcInputProgram := zkcPath + cases[i] + ".zkc"
+			binf, err := compileBinaryConstraints(zkcInputProgram)
 			if err != nil {
 				t.Fatalf("failed to compile zkc source: %v", err)
 			}
 			if files.CheckFilePath(zkcInputPath) != nil {
 				t.Fatalf("zkc input file %s does not exist", zkcInputPath)
 			}
-			binfBytes, err := binf.MarshalBinary()
+			inputBytes, err := os.ReadFile(zkcInputPath)
 			if err != nil {
-				t.Fatalf("failed to marshal binary constraints: %v", err)
+				t.Fatalf("failed to read zkc input file: %v", err)
 			}
-			sys := wiop.NewSystemf("zkc-test/native/%s", cases[i])
-			sys.NewRound()
-			driver := zkcdriver.NewZkCDriver(sys, zkcdriver.Settings{}, bytes.NewReader(binfBytes))
-			inputs := zkcdriver.ReadZkcInputs(zkcInputPath)
-
-			proverCompilePipeline(sys)
-
-			proof, pi := sys.Prove(func(rt *wiop.Runtime) {
-				driver.AssignWithPreRead(rt, inputs)
-			})
-			if err := sys.Verify(proof, pi); err != nil {
-				t.Fatalf("native test failed: %v", err)
+			tc := zkcTestCase{
+				ZkcFilePath: zkcInputPath,
+				InputStr:    string(inputBytes),
+			}
+			inputs, _, err := parseTestCase(tc, binf)
+			if err != nil {
+				t.Fatalf("failed to parse test case: %v", err)
+			}
+			if err := runProveVerify(inputs, binf, proverCompilePipeline); err != nil {
+				t.Fatalf("failed to run test case: %v", err)
 			}
 		})
 	}
