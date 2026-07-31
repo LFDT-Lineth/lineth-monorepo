a186b839005f7a00ecaeffbfb34cdb52e9ba6c53
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-07-31 22:11:41 +0000

    tests: refactor native_module test
    Signed-off-by: Ivo Kubjas <ivo.kubjas@consensys.net>

--- prover-ray/zkcdriver/native_modules_test.go
+++ prover-ray/zkcdriver/native_modules_test.go
@@ -14,30 +14,7 @@ func TestNative(t *testing.T) {
 	cases := []string{"u64", "u128", "u256"}
 	for i := range cases {
 		t.Run(cases[i], func(t *testing.T) {
-			zkcInputPath := zkcPath + cases[i] + ".json"
-			zkcInputProgram := zkcPath + cases[i] + ".zkc"
-			binf, err := compileBinaryConstraints(zkcInputProgram)
-			if err != nil {
-				t.Fatalf("failed to compile zkc source: %v", err)
-			}
-			if files.CheckFilePath(zkcInputPath) != nil {
-				t.Fatalf("zkc input file %s does not exist", zkcInputPath)
-			}
-			inputBytes, err := os.ReadFile(zkcInputPath)
-			if err != nil {
-				t.Fatalf("failed to read zkc input file: %v", err)
-			}
-			tc := zkcTestCase{
-				ZkcFilePath: zkcInputPath,
-				InputStr:    string(inputBytes),
-			}
-			inputs, _, err := parseTestCase(tc, binf)
-			if err != nil {
-				t.Fatalf("failed to parse test case: %v", err)
-			}
-			if err := runProveVerify(inputs, binf, proverCompilePipeline); err != nil {
-				t.Fatalf("failed to run test case: %v", err)
-			}
+			runZkcCase(t, zkcPath+cases[i])
 		})
 	}
 }
@@ -80,4 +57,3 @@ func TestSecp256k1Add(t *testing.T) {
 func TestSecp256k1ScalarMul(t *testing.T) {
 	runZkcCase(t, "testdata/secp256k1_scalarmul_u256")
 }
-
