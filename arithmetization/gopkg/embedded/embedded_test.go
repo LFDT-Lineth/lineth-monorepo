2e6399b6c2d7ed935edca60045f2a4c0de9b1c51
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-08-31 06:59:12 +0000

    test: validate compiled constraint file
    Signed-off-by: Ivo Kubjas <ivo.kubjas@consensys.net>

--- arithmetization/gopkg/embedded/embedded_test.go
+++ arithmetization/gopkg/embedded/embedded_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/LFDT-Lineth/zkc/pkg/zkc/compiler/codegen"
+	"github.com/LFDT-Lineth/zkc/pkg/zkc/constraints"
 )
 
 func TestCompiledBinaryFile(t *testing.T) {
@@ -14,5 +15,10 @@ func TestCompiledBinaryFile(t *testing.T) {
 	}
 	if binfile == nil {
 		t.Fatalf("compiled binary file is nil")
+	}
+	air := binfile.AirConstraints()
+	errs := constraints.Validate(air)
+	if len(errs) > 0 {
+		t.Fatalf("air constraints validation failed: %v", errs)
 	}
 }
