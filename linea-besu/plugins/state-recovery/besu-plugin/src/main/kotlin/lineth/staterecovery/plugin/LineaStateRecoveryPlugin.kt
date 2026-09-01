6aba55630ee28fcec7bfbd961059ccc7ffdeb058
Jones Ho <jones.ho@consensys.net>
2026-09-02 00:12:57 +0800

    chore(linea-besu): declare explicitly the enumerated submodules from besu plugin api as non-transitive api dependencies
    Signed-off-by: Jones Ho <jones.ho@consensys.net>

--- linea-besu/plugins/state-recovery/besu-plugin/src/main/kotlin/lineth/staterecovery/plugin/LineaStateRecoveryPlugin.kt
+++ linea-besu/plugins/state-recovery/besu-plugin/src/main/kotlin/lineth/staterecovery/plugin/LineaStateRecoveryPlugin.kt
@@ -1,3 +1,7 @@
+@file:Suppress(
+  "DEPRECATION",
+) // MiningService is deprecated in Besu 26.8.1; only MiningService.stop() is used, migration is deferred.
+
 package lineth.staterecovery.plugin
 
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
