512dae5a6ee2562de6b688933910a0f4808fd365
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-09-07 12:01:58 +0000

    docs: remove obsolete comment
    Signed-off-by: Ivo Kubjas <ivo.kubjas@consensys.net>

--- prover-ray/zkcdriver/zkcdriver_test.go
+++ prover-ray/zkcdriver/zkcdriver_test.go
@@ -145,15 +145,6 @@ func proverCompilePipeline(sys *wiop.System) {
 	logderivativesum.Compile(sys)
 	localvanishing.Compile(sys)
 	global.Compile(sys)
-	// XXX(ivokub): we have disabled pcs compiler for now as zkc compiler doesn't generate lookup constraints.
-	// in that case we would have columns which are not constrained at all and we would get a panic in the
-	// pcs compiler due to shifts not defined.
-	//
-	// replug when zkc start emitting lookup constraints, see https://github.com/LFDT-Lineth/zkc/issues/2013
-	//
-	// and when replugging, then we should also construct a new wiop.System for verifier to ensure that the
-	// verifier doesn't have access to the prover's internal state, so that we would have a more realistic
-	// test case. We should also do it in the pipeline test then.
 	pcs.Compile(sys)
 }
 
